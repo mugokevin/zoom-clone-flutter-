ef8813c5b103db109de174b3e02c8687f94f8b7e
mugokevin <kevinmugo4@gmail.com>
2022-12-02 05:45:25 +0300

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
+import 'package:zoom_clone/screens/history_meeting_screen.dart';
+import 'package:zoom_clone/screens/meeting_screen.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'package:zoom_clone/widgets/custom_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  List<Widget> pages = [
+    MeetingScreen(),
+    const HistoryMeetingScreen(),
+    const Text('Contacts'),
+    CustomButton(text: 'Log Out', onPressed: (() => AuthMethods().signOut())),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: 'Meet and Chat'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.lock_clock), label: 'Meetings'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: 'Contacts'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 17,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+            primary: buttonColor,
+            minimumSize: Size(double.infinity, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+              side: BorderSide(color: buttonColor),
+            )),
+      ),
+    );
+  }
+}
